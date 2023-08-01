f6393d52375e167340ece9049b03d85ef11449ac
plerks <plerks@163.com>
2023-08-02 07:01:00 +0800

    增加LeetCode2681. 英雄的力量, LeetCode78. 子集

---
+++ LeetCode/中等/78. 子集/Solution78.java
@@ -0,0 +1,71 @@
+/*
+url: https://leetcode.cn/problems/subsets/
+LeetCode题解: https://leetcode.cn/problems/subsets/solutions/2367622/problem-78-zi-ji-by-alworn-skns/
+              https://leetcode.cn/problems/TVdhkn/solutions/2329301/suo-you-zi-ji-by-relaxed-7eavittpjh-jem4/
+              https://leetcode.cn/problems/TVdhkn/solutions/2288876/jing-dian-de-zi-ji-shu-wen-ti-by-nice-sa-5f3t/
+*/
+
+import java.util.*;
+
+public class Solution78 {
+    List<List<Integer>> ans = new ArrayList<>();
+    HashSet<Integer> set = new HashSet<>(); // 用数组好像也行
+
+    public List<List<Integer>> subsets(int[] nums) {
+        mySubsets(nums, 0);
+        return ans;
+    }
+
+    // 调用树是nums.length - 1叉树
+    private void mySubsets(int[] nums, int index) {
+        ans.add(new ArrayList<>(set));
+        for (int i = index; i < nums.length; i++) {
+            set.add(nums[i]);
+            mySubsets(nums, i + 1);
+            set.remove(nums[i]);
+        }
+    }
+
+    public List<List<Integer>> subsets_implementation2(int[] nums) {
+        mySubsets_implementation2(nums, 0);
+        return ans;
+    }
+
+    // 调用树是二分的
+    private void mySubsets_implementation2(int[] nums, int index) {
+        // 每个元素都尝试了是否加入
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(set));
+        }
+        else {
+            mySubsets_implementation2(nums, index + 1); // nums[i]不加入
+            set.add(nums[index]);
+            mySubsets_implementation2(nums, index + 1); // nums[i]加入
+            set.remove(nums[index]);
+        }
+    }
+
+    // 不递归的写法
+    public List<List<Integer>> subsets_implementation3(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int size = ans.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> list = new ArrayList<>(ans.get(j));
+                list.add(nums[i]);
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution78 solu = new Solution78();
+        System.out.println(solu.subsets(new int[]{1,2,3}));
+        solu.ans = new ArrayList<>();
+        solu.set = new HashSet<>();
+        System.out.println(solu.subsets_implementation2(new int[]{1,2,3}));
+        System.out.println(solu.subsets_implementation3(new int[]{1,2,3}));
+    }
+}
---
+++ LeetCode/困难/2681. 英雄的力量/Solution2681.java
@@ -0,0 +1,62 @@
+/*
+url: https://leetcode.cn/problems/power-of-heroes/
+LeetCode参考: https://leetcode.cn/problems/power-of-heroes/solutions/2367175/python3javacgotypescript-yi-ti-yi-jie-pa-lgos/
+我的题解: https://leetcode.cn/problems/power-of-heroes/solutions/2367734/can-kao-ling-yi-wei-de-ti-jie-xie-yi-xia-kciv/
+*/
+
+import java.util.Arrays;
+
+/*
+要对nums的所有非空子集进行计算，每个子集起作用的为最小值和最大值，先排序。
+
+每个以a[i]为最小值的子集对最终的值的贡献分两部分，一部分是选出的子集为{a[i]}时，贡献为ai^3，只有一项，系数为1。
+
+另外一种情况是ai和其它项组合，现求以a[i]为最小值，a[n]为最大值的子集个数。
+
+若子集有2项，则有一种情况{a[i],a[n]}，若有3项，则有Cn-i-1,1种(从(i,n)中选一项)，这样递推下去，一共1+Cn-i-1,1+Cn-i-1,2+...+1 = 2^(n-i-1)种情况(二项式定理)。
+即，以ai为最小值的子集对最终值的贡献为ai*(ai^2+a[i+1]^2+2*a[i+2]^2+...+2^n-i-1*an^2)。
+
+为了计算，从右往左遍历nums，为了计算和其它项组合的部分，需要用到这样变化的项p：0, a[n]^2, 2*a[n]^2 + a[n-1]^2, 4a[n]^2 + 2a[n-1]^2 + a[n-2]^2...
+
+每次累加先加a[i]^3，再加a[i]*p，这两项即为以a[i]为最小值的子集对最终的值的贡献。
+
+另一种思路是，以a[i]为最大值的子集对最终值的贡献为a[i]^3 + a[i]^2 * a[i-1] + ... + 2^(i-1)a[i]^2a[1]。
+此时应从左向右遍历nums，p应该这样变化：0, a[1], 2a[1] + a[2], 4a[1] + 2a[2] + a[3], ...
+*/
+
+public class Solution2681 {
+    public int sumOfPower(int[] nums) {
+        Arrays.sort(nums);
+        long ans = 0;
+        long p = 0;
+        int mod = (int)1e9 + 7;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            long x = nums[i]; // 必须将nums[i]转为long, 否则x * x就会溢出int
+            // ans = ((ans + (x * x % mod) * x) % mod + (p * x % mod)) % mod;
+            ans = (ans + (x * x % mod) * x) % mod;
+            ans = (ans + p * x % mod) % mod;
+            p = (p * 2 % mod + x * x % mod) % mod;
+        }
+        return (int)ans;
+    }
+
+    public int sumOfPower_implementation2(int[] nums) {
+        Arrays.sort(nums);
+        long ans = 0;
+        long p = 0;
+        int mod = (int)1e9 + 7;
+        for (int i = 0; i < nums.length; i++) {
+            long x = nums[i]; // 必须将nums[i]转为long, 否则x * x就会溢出int
+            ans = (ans + (x * x % mod) * x) % mod;
+            ans = (ans + (p * x) % mod * x % mod) % mod;
+            p = (p * 2 % mod + x) % mod;
+        }
+        return (int)ans;
+    }
+
+    public static void main(String[] args) {
+        Solution2681 solu = new Solution2681();
+        System.out.println(solu.sumOfPower(new int[]{1, 2, 4}));
+        System.out.println(solu.sumOfPower_implementation2(new int[]{1, 2, 4}));
+    }
+}
