2fe8972b26243ed7e55d3efedfb180290fb7d17c
plerks <plerks@163.com>
2023-08-19 13:02:47 +0800

    增加LeetCode2235. 两整数相加

---
+++ LeetCode/简单/2235. 两整数相加/Solution2235.java
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/add-two-integers/
+
+public class Solution2235 {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        Solution2235 solu = new Solution2235();
+        System.out.println(solu.sum(1, 2));
+    }
+}
