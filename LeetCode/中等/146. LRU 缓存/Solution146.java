c3b5d359f87d152e2cadeaecbd8c37d6219a54c3
plerks <plerks@163.com>
2023-09-25 14:58:09 +0800

    增加LeetCode460. LFU 缓存, 优化 146. LRU 缓存 代码

--- LeetCode/中等/146. LRU 缓存/Solution146.java
+++ LeetCode/中等/146. LRU 缓存/Solution146.java
@@ -5,30 +5,27 @@ LeetCode参考: https://leetcode.cn/problems/lru-cache/solutions/259678/lruhuan-
 */
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 // 实现方式达不到题目要求的get和put都是O(1)，得自己控制双向链表才能达到O(1)，不过能过LeetCode
 public class Solution146 {
-    List<int[]> buffer; // 数组0为key, 1为value
     int capacity;
-    Map<Integer, int[]> map;
+    Map<Integer, Node> map;
+    DoubleLinkedList list;
 
     // 粘贴到LeetCode上需将这个方法名改为LRUCache
     public Solution146(int capacity) {
         this.capacity = capacity;
-        buffer = new LinkedList<>();
         this.map = new HashMap<>();
+        this.list = new DoubleLinkedList();
     }
 
     public int get(int key) {
         if (map.containsKey(key)) {
-            int[] temp = map.get(key);
-            buffer.remove(temp); // 这里没法O(1)，得自己控制双向链表才能达到O(1)
-            buffer.add(temp); // 最近用过，将其移到最后
-            map.put(temp[0], temp);
-            return temp[1];
+            Node node = map.get(key);
+            list.remove(node);
+            list.addAsFirst(node); // 最近用过，将其移到最前
+            return node.val;
         }
         else {
             return -1;
@@ -37,27 +34,77 @@ public class Solution146 {
 
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            int[] temp = map.get(key);
-            buffer.remove(temp);
-            temp[1] = value;
-            buffer.add(temp);
+            Node node = map.get(key);
+            list.remove(node);
+            list.addAsFirst(node);
+            node.val = value;
             return;
         }
         // 不存在key, 不需淘汰
-        if (buffer.size() < capacity) {
-            int[] newCache = new int[]{key, value};
-            buffer.add(newCache);
-            map.put(key, newCache);
+        if (list.size() < capacity) {
+            Node node = new Node(key, value);
+            list.addAsFirst(node);
+            map.put(key, node);
             return;
         }
         // 不存在key, 需要淘汰
-        int[] removed = buffer.remove(0);
-        map.remove(removed[0]);
-        int[] newCache = new int[]{key, value};
-        buffer.add(newCache);
-        map.put(key, newCache);
+        Node removed = list.removeLast();
+        map.remove(removed.key);
+        Node node = new Node(key, value);
+        list.addAsFirst(node);
+        map.put(key, node);
     }
 
     public static void main(String[] args) {
     }
+}
+
+class Node {
+    Node prev;
+    Node next;
+    int key, val;
+
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+class DoubleLinkedList {
+    Node dummyHead, dummyTail;
+    private int size;
+
+    public DoubleLinkedList() {
+        this.dummyHead = new Node(-1, -1);
+        this.dummyTail = new Node(-1, -1);
+        dummyHead.next = dummyTail;
+        dummyTail.prev = dummyHead;
+        size = 0;
+    }
+
+    public void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        size--;
+    }
+
+    public Node removeLast() {
+        Node node = dummyTail.prev;
+        this.remove(node);
+        return node;
+    }
+
+    public void addAsFirst(Node node) { // 加到头部
+        Node temp = dummyHead.next;
+        dummyHead.next = node;
+        node.prev = dummyHead;
+        temp.prev = node;
+        node.next = temp;
+        size++;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
 }
---
+++ LeetCode/困难/460. LFU 缓存/Solution460.java
@@ -0,0 +1,150 @@
+/*
+url: https://leetcode.cn/problems/lfu-cache/submissions/?envType=daily-question&envId=2023-09-25
+LeetCode参考: https://leetcode.cn/problems/lfu-cache/solutions/186348/lfuhuan-cun-by-leetcode-solution/
+标签: map + 双向链表
+*/
+
+import java.util.HashMap;
+
+public class Solution460 {
+    int capacity;
+    HashMap<Integer, Node> keyMap;
+    HashMap<Integer, DoubleLinkedList> freqMap;
+    int minFreq;
+
+    // 粘贴到LeetCode上需要把这个方法名改为LFUCache
+    public Solution460(int capacity) {
+        this.capacity = capacity;
+        this.keyMap = new HashMap<>();
+        this.freqMap = new HashMap<>();
+        this.minFreq = 0;
+    }
+    
+    public int get(int key) {
+        if (keyMap.containsKey(key)) {
+            Node node = keyMap.get(key);
+            freqMap.get(node.freq).remove(node);
+            if (freqMap.get(node.freq).size() == 0) {
+                if (minFreq == node.freq) {
+                    minFreq += 1;
+                }
+                freqMap.remove(node.freq);
+            }
+            node.freq++;
+            if (!freqMap.containsKey(node.freq)) {
+                freqMap.put(node.freq, new DoubleLinkedList());
+            }
+            freqMap.get(node.freq).addAsFirst(node);
+            return node.val;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (keyMap.containsKey(key)) {
+            Node node = keyMap.get(key);
+            node.val = value;
+            freqMap.get(node.freq).remove(node);
+            if (freqMap.get(node.freq).size() == 0) {
+                if (minFreq == node.freq) {
+                    minFreq += 1;
+                }
+                freqMap.remove(node.freq);
+            }
+            node.freq++;
+            if (!freqMap.containsKey(node.freq)) {
+                freqMap.put(node.freq, new DoubleLinkedList());
+            }
+            freqMap.get(node.freq).addAsFirst(node);
+            return;
+        }
+        // 不存在key，不需淘汰
+        if (keyMap.size() < capacity) {
+            Node node = new Node(key, value, 1);
+            keyMap.put(key, node);
+            if (!freqMap.containsKey(1)) {
+                freqMap.put(1, new DoubleLinkedList());
+            }
+            freqMap.get(1).addAsFirst(node);
+            minFreq = 1;
+            return;
+        }
+        // 不存在key，需要淘汰
+        else {
+            Node node = new Node(key, value, 1);
+            keyMap.put(key, node);
+            DoubleLinkedList list = freqMap.get(minFreq);
+            keyMap.remove(list.removeLast().key); // 同频率，删除最久未使用的
+            if (!freqMap.containsKey(1)) {
+                freqMap.put(1, new DoubleLinkedList());
+            }
+            freqMap.get(1).addAsFirst(node);
+            minFreq = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution460 solu = new Solution460(2);
+        solu.put(1, 1);
+        solu.put(2, 2);
+        System.out.println(solu.get(1));
+        solu.put(3, 3);
+        System.out.println(solu.get(2));
+        System.out.println(solu.get(3));
+        solu.put(4, 4);
+        System.out.println(solu.get(1));
+        System.out.println(solu.get(3));
+        System.out.println(solu.get(4));
+    }
+}
+
+class Node {
+    Node prev;
+    Node next;
+    int key, val, freq;
+
+    public Node(int key, int val, int freq) {
+        this.key = key;
+        this.val = val;
+        this.freq = freq;
+    }
+}
+
+class DoubleLinkedList {
+    Node dummyHead, dummyTail;
+    private int size;
+
+    public DoubleLinkedList() {
+        this.dummyHead = new Node(-1, -1, -1);
+        this.dummyTail = new Node(-1, -1, -1);
+        dummyHead.next = dummyTail;
+        dummyTail.prev = dummyHead;
+        size = 0;
+    }
+
+    public void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        size--;
+    }
+
+    public Node removeLast() {
+        Node node = dummyTail.prev;
+        this.remove(node);
+        return node;
+    }
+
+    public void addAsFirst(Node node) { // 加到头部
+        Node temp = dummyHead.next;
+        dummyHead.next = node;
+        node.prev = dummyHead;
+        temp.prev = node;
+        node.next = temp;
+        size++;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+}
