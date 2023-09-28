50ad76d8d720b1e99223cd5df5a7dcd7b59ee43a
plerks <plerks@163.com>
2023-09-28 12:24:09 +0800

    增加LeetCode2251. 花期内花的数目, 增加典型问题/求一个点被几个区间覆盖/

---
+++ LeetCode/困难/2251. 花期内花的数目/Solution2251.java
@@ -0,0 +1,62 @@
+/*
+url: https://leetcode.cn/problems/number-of-flowers-in-full-bloom/?envType=daily-question&envId=2023-09-28
+LeetCode参考: https://leetcode.cn/problems/number-of-flowers-in-full-bloom/solutions/1444982/by-tsreaper-s39k/
+              https://leetcode.cn/problems/number-of-flowers-in-full-bloom/solutions/1445000/chai-fen-pythonjavacgo-by-endlesscheng-wz35/
+相关: /典型问题/求一个点被几个区间覆盖
+标签: 求一个点被几个区间覆盖
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution2251 {
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        /* 基础想法是t时刻能看到的花的数目为{<=t之前开的花} - {<t时刻凋谢的花}，所以可以分别排序开花和凋谢时间后二分搜索数量。
+        但是参考题解有更好的解题方式，将开花点，凋谢点和人的时间点排序，遍历，用一个变量now表示当前活跃的花，若花开则now++，若是人则当前能看到now朵花，
+        花谢则now-- */
+        List<Point> list = new ArrayList<>();
+        for (int[] arr : flowers) {
+            // 拆成2个点
+            list.add(new Point(arr[0], Integer.MIN_VALUE));
+            list.add(new Point(arr[1], Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < people.length; i++) {
+            list.add(new Point(people[i], i)); // 直接可以通过Point.y找到原来的位置
+        }
+        Collections.sort(list, (p1, p2) -> {
+            if (p1.x != p2.x) {
+                return p1.x < p2.x ? -1 : 1;
+            }
+            if (p1.y != p2.y) {
+                return p1.y < p2.y ? -1 : 1; // 直接写p1.y - p2.y可能p1.y == Integer.MAX_VALUE导致溢出变成正的了
+            }
+            return 0;
+        });
+        /* 若x相同，则要按花开(Integer.MIN_VALUE)，人(i)，花谢(Integer.MAX_VALUE)来处理，同一时刻花开人能看到花，花谢人也能看到，这样计数才是对的。
+        所以y要那样取值，排序后遍历list刚好会是这个处理顺序 */
+        int now = 0;
+        for (Point point : list) {
+            if (point.y == Integer.MIN_VALUE) now++;
+            else if (point.y == Integer.MAX_VALUE) now--;
+            else people[point.y] = now; // 直接复用people
+        }
+        return people;
+    }
+
+    public static void main(String[] args) {
+        Solution2251 solu = new Solution2251();
+        System.out.println(Arrays.toString(solu.fullBloomFlowers(new int[][]{{1,6},{3,7},{9,12},{4,13}}, new int[]{2,3,7,11})));
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ 典型问题/求一个点被几个区间覆盖/IntervalCover.java
@@ -0,0 +1,106 @@
+/*
+给出横轴上的一些闭区间和一系列数，求每个数被几个区间覆盖。
+相关: LeetCode35. 搜索插入位置 https://leetcode.cn/problems/search-insert-position/
+      LeetCode2251. 花期内花的数目 https://leetcode.cn/problems/number-of-flowers-in-full-bloom/
+标签: 求一个点被几个区间覆盖
+*/
+
+import java.util.*;
+
+public class IntervalCover {
+    // 将x包含的区间数量为{起点<=x的区间数量} - {终点<x的区间数量}
+    public int[] getCoverCount(int[][] intervals, int[] xArr) {
+        int n = intervals.length;
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for (int i = 0; i < n; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int[] ans = new int[xArr.length];
+        for (int i = 0; i < xArr.length; i++) {
+            ans[i] = getFirstLargerIndex(start, xArr[i]) - getFirstLargerOrEqualIndex(end, xArr[i]);
+        }
+        return ans;
+    }
+
+    // 找到arr中第一个>x的位置
+    public int getFirstLargerIndex(int[] arr, int x) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > x) {
+                right = mid; // 转入[left, mid]
+            }
+            else {
+                left = mid + 1; // 转入[mid + 1, right]
+            }
+        }
+        return left;
+    }
+
+
+    // 找到arr中第一个>=x的位置
+    public int getFirstLargerOrEqualIndex(int[] arr, int x) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] >= x) {
+                right = mid; // 转入[left, mid]
+            }
+            else {
+                left = mid + 1; // 转入[mid + 1, right]
+            }
+        }
+        return left;
+    }
+
+    // 参考https://leetcode.cn/problems/number-of-flowers-in-full-bloom/solutions/1444982/by-tsreaper-s39k/中的解法
+    public int[] getCoverCount_implementation2(int[][] intervals, int[] xArr) {
+        List<Point> list = new ArrayList<>();
+        for (int[] arr : intervals) {
+            list.add(new Point(arr[0], Integer.MIN_VALUE));
+            list.add(new Point(arr[1], Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < xArr.length; i++) {
+            list.add(new Point(xArr[i], i));
+        }
+        Collections.sort(list, (p1, p2) -> {
+            if (p1.x != p2.x) {
+                return p1.x < p2.x ? -1 : 1;
+            }
+            if (p1.y != p2.y) {
+                return p1.y < p2.y ? -1 : 1; // 直接写p1.y - p2.y可能p1.y == Integer.MAX_VALUE导致溢出变成正的了
+            }
+            return 0;
+        });
+        /* 若x相同，则要按起点(Integer.MIN_VALUE)，x(i)，终点(Integer.MAX_VALUE)来处理，同一时刻区间开始会覆盖x，结束也是这个区间覆盖了x，这样计数才是对的。
+        所以y要那样取值，排序后遍历list刚好会是这个处理顺序 */
+        int now = 0;
+        for (Point point : list) {
+            if (point.y == Integer.MIN_VALUE) now++;
+            else if (point.y == Integer.MAX_VALUE) now--;
+            else xArr[point.y] = now; // 直接复用xArr
+        }
+        return xArr;
+    }
+
+    public static void main(String[] args) {
+        IntervalCover intervalCover = new IntervalCover();
+        System.out.println(Arrays.toString(intervalCover.getCoverCount(new int[][]{{1, 2}, {2, 3}, {5, 6}}, new int[]{2, 3, 7})));
+        System.out.println(Arrays.toString(intervalCover.getCoverCount_implementation2(new int[][]{{1, 2}, {2, 3}, {5, 6}}, new int[]{2, 3, 7})));
+    }
+}
+
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
