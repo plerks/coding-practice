e39b0842411ea7c313cc011e8bf1bf8e261071b0
plerks <plerks@163.com>
2023-10-01 00:15:50 +0800

    增加LeetCode121. 买卖股票的最佳时机

---
+++ LeetCode/简单/121. 买卖股票的最佳时机/Solution121.java
@@ -0,0 +1,18 @@
+// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/?envType=daily-question&envId=2023-10-01
+
+public class Solution121 {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int val : prices) {
+            min = Math.min(val, min);
+            maxProfit = Math.max(val - min, maxProfit);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Solution121 solu = new Solution121();
+        System.out.println(solu.maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+}
