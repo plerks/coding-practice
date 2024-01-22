e576da077005aaf9ef8bafeea73f038f8e03eea3
plerks <plerks@163.com>
2024-01-23 00:26:17 +0800

    增加LeetCode2765. 最长交替子数组

---
+++ LeetCode/简单/2765. 最长交替子数组/Solution2765.java
@@ -0,0 +1,30 @@
+// https://leetcode.cn/problems/longest-alternating-subarray/description/?envType=daily-question&envId=2024-01-23
+
+public class Solution2765 {
+    public int alternatingSubarray(int[] nums) {
+        int ans = 0;
+        int len = 1, currentDiff = -1;
+        for (int i = 1; i < nums.length; i++) {
+            int d = nums[i] - nums[i - 1];
+            if (d == -currentDiff) { // 在交替
+                len++;
+                ans = Math.max(ans, len);
+                currentDiff = -currentDiff;
+            }
+            else if (d == 1) { // 没交替，但是可以作为新的开头
+                len = 2;
+                currentDiff = 1;
+            }
+            else {
+                len = 1;
+                currentDiff = -1;
+            }
+        }
+        return ans > 1 ? ans : -1;
+    }
+
+    public static void main(String[] args) {
+        Solution2765 solu = new Solution2765();
+        System.out.println(solu.alternatingSubarray(new int[]{1,2,2,1,2,3,6,2}));
+    }
+}
