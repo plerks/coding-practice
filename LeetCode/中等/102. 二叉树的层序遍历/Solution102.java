4825d6502ba805978b0a09a49830bf4e9894e94d
plerks <plerks@163.com>
2024-02-16 10:19:30 +0800

    增加LeetCode103. 二叉树的锯齿形层序遍历

--- LeetCode/中等/102. 二叉树的层序遍历/Solution102.java
+++ LeetCode/中等/102. 二叉树的层序遍历/Solution102.java
@@ -1,6 +1,6 @@
 /*
 url: https://leetcode.cn/problems/binary-tree-level-order-traversal/description/?envType=daily-question&envId=2024-02-14
-相关: LeetCode107. 二叉树的层序遍历 II
+相关: LeetCode107. 二叉树的层序遍历 II, LeetCode103. 二叉树的锯齿形层序遍历
 标签: 层序遍历
 */
 
--- LeetCode/中等/103. 二叉树的锯齿形层序遍历/Solution103.java
+++ LeetCode/中等/103. 二叉树的锯齿形层序遍历/Solution103.java
@@ -1,33 +1,35 @@
 /*
-url: https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/description/?envType=daily-question&envId=2024-02-15
-相关: LeetCode102. 二叉树的层序遍历, LeetCode103. 二叉树的锯齿形层序遍历
+url: https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/description/?envType=daily-question&envId=2024-02-16
+相关: LeetCode102. 二叉树的层序遍历, LeetCode107. 二叉树的层序遍历 II
 标签: 层序遍历
 */
 
 import java.util.*;
 
-public class Solution107 {
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+public class Solution103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         Deque<TreeNode> q = new LinkedList<>();
         List<List<Integer>> ans = new ArrayList<>();
         if (root != null) q.offer(root);
+        int level = -1;
         while (!q.isEmpty()) {
             int size = q.size();
             List<Integer> list = new ArrayList<>();
+            level++;
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
                 list.add(node.val);
                 if (node.left != null) q.offer(node.left);
                 if (node.right != null) q.offer(node.right);
             }
+            if (level % 2 == 1) Collections.reverse(list);
             ans.add(list);
         }
-        Collections.reverse(ans);
         return ans;
     }
 
     public static void main(String[] args) {
-        Solution107 solu = new Solution107();
-        System.out.println(solu.levelOrderBottom(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+        Solution103 solu = new Solution103();
+        System.out.println(solu.zigzagLevelOrder(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
     }
 }
