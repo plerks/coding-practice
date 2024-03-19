a89d0edad0ae2c66e30c9062cea6ec7249f26737
plerks <plerks@163.com>
2024-03-19 18:31:01 +0800

    增加LeetCode303. 区域和检索 - 数组不可变

---
+++ LeetCode/简单/303. 区域和检索 - 数组不可变/Solution303.java
@@ -0,0 +1,28 @@
+/*
+url: https://leetcode.cn/problems/range-sum-query-immutable/description/?envType=daily-question&envId=2024-03-18
+标签: 前缀和
+*/
+
+public class Solution303 {
+    int[] prefixSum;
+    int[] nums;
+
+    // 粘贴到LeetCode上把这个方法名改为NumArray
+    public Solution303(int[] nums) {
+        int n = nums.length;
+        this.nums = nums;
+        this.prefixSum = new int[n];
+        for (int i = 0; i < n; i++) {
+            prefixSum[i] = (i == 0) ? nums[i] : nums[i] + prefixSum[i - 1];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return prefixSum[right] - prefixSum[left] + nums[left];
+    }
+
+    public static void main(String[] args) {
+        Solution303 solu = new Solution303(new int[]{-2, 0, 3, -5, 2, -1});
+        System.out.println(solu.sumRange(0, 2));
+    }
+}
