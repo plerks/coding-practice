cbc3a7bedde541392fb4d79c946fdda044683124
plerks <plerks@163.com>
2024-03-21 20:39:33 +0800

    补充LeetCode84. 柱状图中最大的矩形将两次单调栈合并为一次的做法

--- LeetCode/困难/84. 柱状图中最大的矩形/Solution84.java
+++ LeetCode/困难/84. 柱状图中最大的矩形/Solution84.java
@@ -1,5 +1,6 @@
 /*
 url: https://leetcode.cn/problems/largest-rectangle-in-histogram/description/
+LeetCode参考: https://leetcode.cn/problems/largest-rectangle-in-histogram/solutions/266844/zhu-zhuang-tu-zhong-zui-da-de-ju-xing-by-leetcode-/
 相关: LeetCode1793. 好子数组的最大分数, LeetCode42. 接雨水, LeetCode2334. 元素值大于变化阈值的子数组, LeetCode907. 子数组的最小值之和
 标签: 单调栈, 子数组的最小值
 */
@@ -39,8 +40,38 @@ public class Solution84 {
         return ans;
     }
 
+    /* 参考官方题解，将两次单调栈合并为一次，但是要做一点妥协。考虑从左到右求左侧第一个小于heights[i]的那次循环，当i使得单调栈中元素x出栈时，
+    此时有heights[x] >= heights[i]，且(x,i)中间的元素都是大于heights[x]的，否则x应该早已经在这些元素的循环轮中被弹出，因此此时i是出栈元素x
+    右侧第一个**小于等于**height[x]的位置。虽然这里是小于等于，但是整体并不会导致错误，因为当多个等高的柱子并排时，最大的矩形面积会在最右边那
+    个柱子那里算出来。
+    */
+    public int largestRectangleArea_implementation2(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n]; // 用单调栈求出nums[i]左侧第一个**小于**nums[i]的位置
+        int[] right = new int[n]; // 顺便求出nums[i]右侧第一个**小于等于**nums[i]的位置
+        Arrays.fill(right, n);
+        Deque<Integer> stack = new LinkedList<>();
+        // 单次循环即可
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                int x = stack.pop();
+                right[x] = i;
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int low = left[i];
+            int high = right[i];
+            ans = Math.max(ans, heights[i] * (high - 1 - (low + 1) + 1));
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Solution84 solu = new Solution84();
         System.out.println(solu.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        System.out.println(solu.largestRectangleArea_implementation2(new int[]{2,1,5,6,2,3}));
     }
 }
