5c7c3bc818cac8e775e8303065c52f9d4ca4ab3a
plerks <plerks@163.com>
2024-10-21 23:43:28 +0800

    补充一个大小写反转的函数

--- 典型问题/大小写转换/UpperLowerCaseConversion.java
+++ 典型问题/大小写转换/UpperLowerCaseConversion.java
@@ -14,11 +14,19 @@ public class UpperLowerCaseConversion {
     public char toLower(char c) {
         return (char)(c | mask); // 把第6位(从1开始计数)变为1，其它位不变
     }
+
+    // 若是小写则转大写，若是大写则转小写。0是异或运算的单位元，与0异或不变，而与1异或则会反转
+    public char transfer(char c) {
+        return (char)(c ^ 0x20);
+    }
+
     public static void main(String[] args) {
         UpperLowerCaseConversion ulcc = new UpperLowerCaseConversion();
-        System.out.println(ulcc.toUpper('c'));
-        System.out.println(ulcc.toUpper('C'));
-        System.out.println(ulcc.toLower('c'));
-        System.out.println(ulcc.toLower('C'));
+        System.out.println(ulcc.toUpper('a'));
+        System.out.println(ulcc.toUpper('A'));
+        System.out.println(ulcc.toLower('a'));
+        System.out.println(ulcc.toLower('A'));
+        System.out.println(ulcc.transfer('a'));
+        System.out.println(ulcc.transfer('A'));
     }
 }
