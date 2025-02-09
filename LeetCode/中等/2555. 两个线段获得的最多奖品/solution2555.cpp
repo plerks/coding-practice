f6a373d908e85890d159b5256c2b3b595a340088
gxy <plerks@163.com>
2025-02-09 13:03:44 +0800

    解决`LeetCode2555`里的待做。更新ans的位置不对会导致wa的问题 更新ans的位置得放外面，不然有可能：     右侧滑窗r2可以变大，l2不变，此时sum2变大了，但是左滑窗没有增加新的选择可能，     while (r1 < l2)进不去，这时就没有去计算更新ans

--- LeetCode/中等/2555. 两个线段获得的最多奖品/solution2555.cpp
+++ LeetCode/中等/2555. 两个线段获得的最多奖品/solution2555.cpp
@@ -11,11 +11,11 @@ using namespace std;
 
 class Solution2555 {
 public:
-    /* 我的做法，68 / 72 个通过的测试用例，但是不知道为什么错，待做: 为什么这样写不对？
+    /* 我的做法，
     分两个区间相交和不相交来考虑，区间不相交时，是两个段，考虑第二个段，然后看前一个段能取到的最大值，
     区间相交时，是个区间最长2k的单滑窗，尽量包含多的位置，因此贪心地取长为2k，但是实际不需要特别考虑相交的情况。
     */
-    int maximizeWin_wa(vector<int>& prizePositions, int k) {
+    int maximizeWin(vector<int>& prizePositions, int k) {
         int n = prizePositions.size();
         // 两个区间不相交，题意两个区间长度都必须为k。两个区间相交的情况一定更小，不用管，注意x轴无限长，不存在不得不相交的情况
         // 最优解一定可以满足区间右端点在coins某个位置上对齐，类似LeetCode2271. 毯子覆盖的最多白色砖块数
@@ -26,6 +26,7 @@ public:
         int l1 = 0, r1 = 0;
         int ans = 0;
         // 注意，最开始比如[l2, r2]在[0, 0]处时，不代表不满足题目要求的区间 r - l == k，此时l2相当于在负数位置，所以刚开始时长度不够k的问题不必太在意
+        // 这里虽然有多层循环，但是是2段在滑窗，时间复杂度是O(n)
         for (int r2 = 0; r2 < n; r2++) {
             // 即便prizePositions有重复元素，比如3 3 3，现在没到最右边那个3不要紧，后面会到
             sum2++;
@@ -40,14 +41,14 @@ public:
                     l1++;
                 }
                 mx = max(mx, sum1);
-                ans = max(ans, sum2 + mx);
                 r1++;
             }
+            ans = max(ans, sum2 + mx);
         }
         return ans;
     }
 
-    int maximizeWin(vector<int>& prizePositions, int k) {
+    int maximizeWin_implementation2(vector<int>& prizePositions, int k) {
         int n = prizePositions.size();
         int l = 0;
         int ans = 0;
@@ -66,5 +67,6 @@ public:
 int main(int argc, char const *argv[]) {
     Solution2555 solu;
     cout << solu.maximizeWin(*new vector<int>{1,1,2,2,3,3,5}, 2) << endl;
+    cout << solu.maximizeWin_implementation2(*new vector<int>{1,1,2,2,3,3,5}, 2) << endl;
     return 0;
 }
